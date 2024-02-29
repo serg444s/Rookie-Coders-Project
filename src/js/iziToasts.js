1bfecbecfcd2fd27f1a1a8fca2b9801e2bf9d832
ChernyshovVadym <vad14@i.ua>
2024-02-29 19:25:10 +0200

    add

--- src/js/iziToasts.js
+++ src/js/iziToasts.js
@@ -2,11 +2,8 @@ import iziToast from 'izitoast';
 
 import 'izitoast/dist/css/iziToast.min.css';
 
-export const gallery = document.querySelector('.gallery');
-export const userInput = document.querySelector('input');
-
 export const BOOKS =
-  'Sorry, there are no images matching your search query. Please try again!';
+  'Sorry, there are no books matching your search query. Please try again!';
 
 export function onError(message) {
   iziToast.error({
