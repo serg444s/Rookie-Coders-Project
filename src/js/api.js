ff8b9662458a1cb5f296738760606e6603e265f8
serg444s <143083342+serg444s@users.noreply.github.com>
2024-02-29 11:54:19 +0200

    partials

---
+++ src/js/api.js
@@ -0,0 +1,3 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://books-backend.p.goit.global/books/';
---
+++ src/js/iziToasts.js
@@ -0,0 +1,2 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
