18d561ec8bf23189be4d14a002efc9a2a3ce2d15
Danik005 <141757644+HUWTUCH@users.noreply.github.com>
2024-03-02 14:01:14 +0200

    commit books two

--- src/js/startLoad.js
+++ src/js/startLoad.js
@@ -7,11 +7,11 @@ export default async function startLoad() {
   const categories = document.querySelector('.top-categories-list');
   // try {
   //   const response = await axios.get('https://books-backend.p.goit.global/books/category-list');
-  //   categories.insertAdjacentHTML('beforeend', await  murkupCategoryList(response));
+  //   categories.insertAdjacentHTML('beforeend',  murkupCategoryList(response));
   // } catch (error) {
   //   console.log(error.message);
   //   onError();
-  // } це не потрібно
+  // } Ось цей try...catch не потрібний
   try {
     const resp = await axios.get('https://books-backend.p.goit.global/books/top-books');
     categories.insertAdjacentHTML('afterbegin','<h2 class="categories-title">Best Sellers<span> Books</span></h2>');
