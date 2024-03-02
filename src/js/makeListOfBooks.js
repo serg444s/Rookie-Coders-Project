e71cf8ea9e61f0196fbe9e779b39b7ec74682cb8
valiana35 <144236658+valiana35@users.noreply.github.com>
2024-03-02 21:02:12 +0200

    modal-window

--- src/js/makeListOfBooks.js
+++ src/js/makeListOfBooks.js
@@ -3,7 +3,7 @@ export async function makeListOfBooks(data) {
     .map(({ author, book_image, title, description, _id }) => {
       return `<li class="books-item-about" id=${_id} >  
     <div class="book-wrap"> 
-    <img class="book-img" src="${book_image}"  alt="${description}"/> 
+    <img class="book-img" src="${book_image}"  alt="${description}" /> 
     </div> 
     <div class="book-info">  
     <p class="info-title">${title}</p>  
--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 import { getCategoriesAPI } from './js/api-catigories-list.js';
 import { onLoad } from './js/dark-mode.js';
 import { startLoad } from './js/startLoad.js';
+import { showModal } from './js/modal-window.js';
+
+const bookList = document.querySelector('.top-categories-list');
+
 getCategoriesAPI();
 
 document.addEventListener('DOMContentLoaded', onLoad);
+bookList.addEventListener('click', showModal);
 
 startLoad();
 
