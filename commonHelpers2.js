7893a479173915746d78f72943841fe059b7dfbd
serg444s <143083342+serg444s@users.noreply.github.com>
2024-03-02 14:31:20 +0000

    Deploying to gh-pages from @ serg444s/Rookie-Coders-Project@64cfb688aab91f5e3d6e37a499d768532b75edc8 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -8,7 +8,7 @@ import"./assets/styles-90b7f055.js";import{a as b}from"./assets/vendor-8cce9181.
 </div>
 <button class="remove-shop-list-book" data-bookid="${d}">
 <svg class="icon-basket-shop-list" width="28" height="26" data-bookid="${d}">
- <use data-name="icon-removebook" href="./img/symbol-defs.svg#icon-removebook"></use>
+ <use data-name="icon-removebook" href="/img/symbol-defs.svg#icon-removebook"></use>
 </svg>
 </button>
 </div>
@@ -19,16 +19,16 @@ import"./assets/styles-90b7f055.js";import{a as b}from"./assets/vendor-8cce9181.
 <a class="amazon-link" target="_blank" href="${i.find(l=>l.name==="Amazon").url}">
 <img 
 class="amazon-img"
-srcset="./img/shopping_list/amazon1x.png 1x,
-./img/shopping_list/amazon2x.png 2x"
-src="./img/shopping_list/amazon1x.png"
+srcset="/img/shopping_list/amazon1x.png 1x,
+/img/shopping_list/amazon2x.png 2x"
+src="/img/shopping_list/amazon1x.png"
 alt="Amazon Shop"
 />
 </a>
 <a class="openbooks-link" target="_blank" href="${i.find(l=>l.name==="Apple Books").url}">
      <img 
   class="open-book-icon"
-  src="./img/shopping_list/openbook.png"
+  src="/img/shopping_list/openbook.png"
   alt="Open book"
   />
   </a>
