4f0bf1534f02b88e090cc419355cf2f2e672ae3e
Danik005 <141757644+HUWTUCH@users.noreply.github.com>
2024-03-02 13:58:50 +0200

    commit books

--- src/js/startLoad.js
+++ src/js/startLoad.js
@@ -3,30 +3,20 @@ import { onError } from './iziToasts';
 import { murkupCategoryList } from './murkupCategoryList';
 import { murkup } from './murkup';
 
-export async function startLoad() {
+export default async function startLoad() {
   const categories = document.querySelector('.top-categories-list');
-
+  // try {
+  //   const response = await axios.get('https://books-backend.p.goit.global/books/category-list');
+  //   categories.insertAdjacentHTML('beforeend', await  murkupCategoryList(response));
+  // } catch (error) {
+  //   console.log(error.message);
+  //   onError();
+  // } це не потрібно
   try {
-    const response = await axios.get(
-      'https://books-backend.p.goit.global/books/category-list'
-    );
-    categories.insertAdjacentHTML('beforeend', murkupCategoryList(response));
-  } catch (error) {
-    console.log(error.message);
-    onError();
-  }
-  try {
-    const resp = await axios.get(
-      'https://books-backend.p.goit.global/books/top-books'
-    );
-    categories.insertAdjacentHTML(
-      'afterbegin',
-      '<h2 class="categories-title">Best Sellers<span> Books</span></h2>'
-    );
-
-    categories
-      .insertAdjacentHTML('beforeend', await murkup(resp.data))
-      .join('');
+    const resp = await axios.get('https://books-backend.p.goit.global/books/top-books');
+    categories.insertAdjacentHTML('afterbegin','<h2 class="categories-title">Best Sellers<span> Books</span></h2>');
+    const markupData = await murkup(resp.data);
+    categories.insertAdjacentHTML('beforeend', markupData)
     return resp.data;
   } catch (error) {
     console.log(error.message);
