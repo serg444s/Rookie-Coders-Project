c6109550b139e107dbc58e1a90ff6085b0c024d2
serg444s <143083342+serg444s@users.noreply.github.com>
2024-03-02 21:33:07 +0200

    modal

--- src/js/modal-window.js
+++ src/js/modal-window.js
@@ -9,17 +9,25 @@ const modalBtn = document.querySelector('.modal-btn');
 const modalCongratText = document.querySelector('.modal-congrat-text');
 
 bookList.addEventListener('click', showModal);
-modalBtn.addEventListener('click', addToShoppingList);
+///закоментував і переніс цей рядок на 47
+// modalBtn.addEventListener('click', addToShoppingList);
 
 let instance;
 export function showModal(event) {
-    event.preventDefault();
-    if (event.target.nodeName !== "IMG") return;
-    const liBook = event.target.closest('li');
-    const id = +liBook.dataset.id;
-    const book = bookList.find(el => el.id === id);
-    const { book_image, title, author, description, buy_links: { url, name }, } = book;
-    instance = basicLightbox.create(`
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') return;
+  const liBook = event.target.closest('li');
+  const id = +liBook.dataset.id;
+  const book = bookList.find(el => el.id === id);
+  const {
+    book_image,
+    title,
+    author,
+    description,
+    buy_links: { url, name },
+  } = book;
+  instance = basicLightbox.create(
+    `
     <div class="modal">
     <button type="button" class="modal-icon">
     <svg class="modal-icon-closed"><use href="../img/symbol-defs.svg#icon-closed"></use></svg>
@@ -33,43 +41,45 @@ export function showModal(event) {
         <button class="modal-btn" type="button">ADD TO SHOPPING LIST</button>
         <p class="modal-congrat-text"></p>
     </div>`,
-{
-    onShow: () => {
-      document.addEventListener('keydown', onImageKeydown);
-    },
-    onClose: () => {
-      document.removeEventListener('keydown', onImageKeydown);
-      modalIcon.addEventListener('click', onModalIconClick);
-      backdrop.addEventListener('click', onBackdropClick);
-    },
-},
-);
-instance.show();
+    {
+      onShow: () => {
+        document.addEventListener('keydown', onImageKeydown);
+        modalBtn.addEventListener('click', addToShoppingList);
+      },
+      onClose: () => {
+        document.removeEventListener('keydown', onImageKeydown);
+        modalIcon.addEventListener('click', onModalIconClick);
+        backdrop.addEventListener('click', onBackdropClick);
+      },
+    }
+  );
+  instance.show();
 }
 
-  function onImageKeydown(event) {
-    event.preventDefault();
-    if (event.code === "Escape") {
-      instance.close();
-    }
-  }
-
-  function onModalIconClick(event) {
-    event.preventDefault();
+function onImageKeydown(event) {
+  event.preventDefault();
+  if (event.code === 'Escape') {
     instance.close();
   }
+}
 
-  function onBackdropClick(event) {
-    event.preventDefault();
-    instance.close();
-  }
+function onModalIconClick(event) {
+  event.preventDefault();
+  instance.close();
+}
+
+function onBackdropClick(event) {
+  event.preventDefault();
+  instance.close();
+}
 
 function addToShoppingList(event) {
   event.preventDefault();
   modalBtn.textContent = 'REMOVE FROM THE SHOPPING LIST';
-  modalCongratText.textContent = 'Congratulations! You have added the book to the shopping list. To delete, press the button "Remove from the shopping list".';
+  modalCongratText.textContent =
+    'Congratulations! You have added the book to the shopping list. To delete, press the button "Remove from the shopping list".';
   addBookIdToStorage();
   modalBtn.addEventListener('click', () => {
-  removeBookIdFromStorage();
+    removeBookIdFromStorage();
   });
 }
