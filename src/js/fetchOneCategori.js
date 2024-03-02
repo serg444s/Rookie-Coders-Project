90d8eaa498ec567ce550275c4458a98c16d2409d
serg444s <143083342+serg444s@users.noreply.github.com>
2024-03-02 18:56:21 +0200

    fetchOneCategori

--- src/js/fetchOneCategori.js
+++ src/js/fetchOneCategori.js
@@ -1,18 +1,49 @@
 import axios from 'axios';
 import { onError } from './iziToasts';
-import { murkup } from './murkup';
 
-fetchOneCategori(category);
-async function fetchOneCategori(category) {
-  const categories = document.querySelector('.categories');
+// для перевірки розкоментуй виклик фукнції
+// fetchOneCategori('Advice How-To and Miscellaneous');
+
+export async function fetchOneCategori(category) {
+  const categories = document.querySelector('.top-categories-list');
+  console.log(categories);
   categories.innerHTML = '';
   try {
-    const response = await axios.get(
+    const resp = await axios.get(
       `https://books-backend.p.goit.global/books/category?category=${category}`
-    ).data;
-    categories.insertAdjacentHTML('beforeend', oneCategoryList(response));
+    );
+    console.log(resp);
+    const data = resp.data;
+
+    categories.insertAdjacentHTML(
+      'afterbegin',
+      await makeCategoryPage(category, data)
+    );
   } catch (error) {
     console.log(error.message);
     onError();
   }
+}
+
+async function makeCategoryPage(category, data) {
+  return ` 
+  <h3 class="book-categoty-title">${category}</h3> 
+  <ul class='list-books'>${await makeListBook(data)}</ul>`;
+}
+
+async function makeListBook(data) {
+  console.log(data);
+  return data.map(makeMarcup).join('');
+}
+
+function makeMarcup({ author, book_image, title, description, _id }) {
+  return `<li class="category-item" id=${_id}>  
+    <div class="book-wrap"> 
+    <img class="book-img" src="${book_image}"  alt="${description}"/> 
+    </div > 
+    <div class="book-info">  
+    <p class="info-title">${title}</p>  
+    <p class="info-author">${author}</p>  
+    </div>  
+    </li>`;
 }
--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import { getCategoriesAPI } from './js/api-catigories-list.js';
 import { onLoad } from './js/dark-mode.js';
 import { startLoad } from './js/startLoad.js';
+
 getCategoriesAPI();
 
 document.addEventListener('DOMContentLoaded', onLoad);
 
 startLoad();
 
-//  додав startLoad, makeListOfBooks, murkup, murkupCategoryList,
+
