88096990dc5b57710efdee309a0587cb50ce8d3d
Makukhinanataliia <makukhinanataliia@gmail.com>
2024-03-03 19:15:19 +0330

    burger

---
+++ src/js/burger.js
@@ -0,0 +1,18 @@
+
+const openModalBtn = document.querySelector('[data-action="open-modal"]');
+const closeModalBtn = document.querySelector('[data-action="close-modal"]');
+const body = document.querySelector('body');
+
+export function onOpenModal() {
+  document.body.classList.add('show-modal');
+  closeModalBtn.style.display = 'flex';
+    openModalBtn.style.display = 'none';
+    body.style.overflow = 'hidden';
+}
+
+export function onCloseModal() {
+  document.body.classList.remove('show-modal');
+  closeModalBtn.style.display = 'none';
+    openModalBtn.style.display = 'flex';
+    body.style.overflow = 'auto';
+}
