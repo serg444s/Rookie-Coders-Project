1cf5a3c38dec146835a148d56d465113bfed1d45
Darialucky <144655337+Darialucky@users.noreply.github.com>
2024-03-03 11:37:56 +0200

    js

--- src/main.js
+++ src/main.js
@@ -2,14 +2,12 @@ import { getCategoriesAPI } from './js/api-catigories-list.js';
 import { onLoad } from './js/dark-mode.js';
 import { startLoad } from './js/startLoad.js';
 import { fetchOneCategori } from './js/fetchOneCategori.js';
-
+import { charities } from './js/support';
 import { showModal } from './js/modal-window.js';
 
 const bookList = document.querySelector('.top-categories-list');
 
-
 // fetchOneCategori('Advice How-To and Miscellaneous');
-
 
 getCategoriesAPI();
 
