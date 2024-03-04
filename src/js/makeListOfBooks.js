d51fb770c437063e8c098a971ca9054a30c10c4b
Danik005 <141757644+HUWTUCH@users.noreply.github.com>
2024-03-05 01:21:22 +0200

    commit

--- src/js/makeListOfBooks.js
+++ src/js/makeListOfBooks.js
@@ -1,14 +1,15 @@
 export async function makeListOfBooks(data) {
   return data
      .map(({ author, book_image, title, description, _id}) => {
-      return `<li class="books-item-about" >  
-    <div class="book-wrap"> 
-    <img class="book-img" src="${book_image}" data-id="${_id}"  alt="${description}" /> 
-    </div> 
-    <div class="book-info">  
-    <p class="info-title">${title}</p>  
-    <p class="info-author">${author}</p>  
-    </div>  
+      return `<li class="books-item-about" >
+    <div class="book-wrap">
+    <img class="book-img" src="${book_image}" data-id="${_id}"  alt="${description}" />
+    <p class="book_cover">QUICK VIEW</p>
+    </div>
+    <div class="book-info">
+    <p class="info-title">${title}</p>
+    <p class="info-author">${author}</p>
+    </div>
     </li>`;
     })
     .join('');
