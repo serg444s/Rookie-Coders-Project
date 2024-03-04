5f4d67d4d7f5ba6dd26d0b0c0d9bd378364a34d0
valiana35 <144236658+valiana35@users.noreply.github.com>
2024-03-04 20:39:13 +0200

    modal-window

--- src/js/modal-window.js
+++ src/js/modal-window.js
@@ -5,8 +5,6 @@ import { removeBookIdFromStorage } from './removeBookIdFromStorage';
 import { getBookById } from './getTopListBooks';
 
 const bookList = document.querySelector('.top-categories-list');
-const modalIcon = document.querySelector('.modal-icon');
-const backdrop = document.querySelector('.backdrop');
 const modalBtn = document.querySelector('.modal-btn');
 const modalCongratText = document.querySelector('.modal-congrat-text');
 
@@ -30,25 +28,34 @@ export async function showModal(event) {
         <h3 class="modal-title">${title}</h3>
         <p class="modal-author">${author}</p> 
         <p class="modal-text">${description}</p>
-        <ul><li class="modal-link"><a href="${amazon_product_url}">Amazon
-        </a></li></ul>
+        <ul><li class="modal-link-icons"><a class="modal-link" href="${amazon_product_url}" target="_blank"><img src="../img/shopping_list/amazon1x.png" alt="applebook" target="_blank" /></a>
+        <img class="modal-link-img" src="../img/shopping_list/openbook.png" />
+        </li></ul>
         </div>
         </div>
         <button class="modal-btn" type="button">ADD TO SHOPPING LIST</button>
         <p class="modal-congrat-text"></p>
     </div>`,
     {
-      onShow: () => {
+      onShow: (instance) => {
         document.addEventListener('keydown', onImageKeydown);
-        // modalBtn.addEventListener('click', addToShoppingList);
+        instance.element().querySelector('.modal-icon').onclick = instance.close;
+        instance.element().querySelector('.modal-btn').onclick =
+        addBookIdToStorage(bookId);
+        instance.element().querySelector('.modal-btn').onclick = instance.element().querySelector('.modal-btn').innerHTML = 'REMOVE ';
+        instance.element().querySelector('.modal-btn').onclick = instance.element().querySelector('.modal-congrat-text').textContent =
+          'Congratulations! You have added the book to the shopping list. To delete, press the button "Remove from the shopping list".';
+        if (instance.element().querySelector('.modal-btn').textContent === 'REMOVE FROM THE SHOPPING LIST') {
+          instance.element().querySelector('.modal-btn').onclick = removeBookIdFromStorage
+        }
       },
       onClose: () => {
         document.removeEventListener('keydown', onImageKeydown);
-        // modalBtn.removeEventListener('click', addToShoppingList);
+        // document.modalBtn.removeEventListener('click', addToShoppingList);
         // modalIcon.addEventListener('click', onModalIconClick);
-        // backdrop.addEventListener('click', onBackdropClick);
       },
-    }
+    },
+    // document.modalBtn.addEventListener('click', addToShoppingList),
   );
   instance.show();
 }
@@ -60,28 +67,22 @@ function onImageKeydown(event) {
   }
 }
 
-function onModalIconClick(event) {
-  event.preventDefault();
-  instance.close();
-}
-
 function onBackdropClick(event) {
   event.preventDefault();
   instance.close();
 }
 
-function addToShoppingList(event) {
-  event.preventDefault();
-  addBookIdToStorage(bookId);
-  modalBtn.textContent = 'REMOVE FROM THE SHOPPING LIST';
-  modalCongratText.textContent =
-    'Congratulations! You have added the book to the shopping list. To delete, press the button "Remove from the shopping list".';
-  if (modalBtn.textContent === 'REMOVE FROM THE SHOPPING LIST') {
-    modalBtn.addEventListener('click', () => {
-      removeBookIdFromStorage(bookIdToRemove);
-    });
-  }
-}
+// function addToShoppingList(event) {
+//   event.preventDefault();
+//   // showModal(event);
+//   addBookIdToStorage();
+//   showModal.modalBtn.textContent = 'REMOVE FROM THE SHOPPING LIST';
+//   showModal.modalCongratText.textContent =
+//     'Congratulations! You have added the book to the shopping list. To delete, press the button "Remove from the shopping list".';
+//   if (modalBtn.textContent === 'REMOVE FROM THE SHOPPING LIST') {
+//     modalBtn.addEventListener('click', () => {
+//       removeBookIdFromStorage(bookIdToRemove);
+//     });
+//   }
+// }
 
-
-/* <li class="modal-link"><a href="${buy_links[url]}">${buy_links[name]}</a></li> */
