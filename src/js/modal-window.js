a6fc01d536ee0ce91b02cf0131fcfd28008441b3
valiana35 <144236658+valiana35@users.noreply.github.com>
2024-03-07 13:53:46 +0200

    modal-window

--- src/js/modal-window.js
+++ src/js/modal-window.js
@@ -6,13 +6,13 @@ import { booksIdArray } from './addBookIdToStorage';
 import amazon1x from '../img/shopping_list/amazon1x.png';
 import amazon2x from '../img/shopping_list/amazon2x.png';
 import openbook from '../img/shopping_list/openbook.png';
-import symbol from '../img/symbol-defs.svg#icon-closed';
 
 const bookList = document.querySelector('.top-categories-list');
 bookList.addEventListener('click', showModal);
 
 let bookId;
 let instance;
+
 export async function showModal(event) {
   event.preventDefault();
   if (event.target.nodeName !== 'IMG') return;
@@ -25,10 +25,6 @@ export async function showModal(event) {
   if (booksIdArray.includes(bookId)) {
     buttonText = 'REMOVE FROM THE SHOPPING LIST';
   }
-  // <svg class="modal-icon-closed" width="18" height="18">
-  //   <use href="../img/symbol-defs.svg#icon-closed"></use>
-  // </svg>;
-
   const {
     book_image,
     title,
@@ -40,7 +36,7 @@ export async function showModal(event) {
   instance = basicLightbox.create(
     `<div class="modal">
         <button type="button" class="modal-icon">
-        <svg class="modal-icon-closed" width="28" height="28" viewBox="0 0 28 28" xmlns="http://www.w3.org/2000/svg">
+        <svg class="modal-icon-closed" width="24" height="24" viewBox="0 0 28 28" xmlns="http://www.w3.org/2000/svg">
   <path d="M21 7L7 21M7 7L21 21" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" />
 </svg>
         </button>
@@ -122,6 +118,7 @@ function changeBookText() {
     instance.element().querySelector('.modal-congrat-text').textContent =
       'Congratulations! You have added the book to the shopping list. To delete, press the button "Remove from the shopping list".';
     addBookIdToStorage(bookId);
+    
   } else {
     instance.element().querySelector('.modal-btn').onclick = instance
       .element()
